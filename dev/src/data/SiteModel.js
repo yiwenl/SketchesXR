38c3e662a07b2431d89dcd02c085243503b51f12
Yi-Wen Lin <bongiovi015@gmail.com>
2021-06-04 14:15:03 +0100

    add portal to front page

--- dev/src/data/SiteModel.js
+++ dev/src/data/SiteModel.js
@@ -5,6 +5,12 @@ const ExpModel = [
     url: "//yiwenl.github.io/SketchesXR/experiments/typography01/dist/index.html",
     title: "Typography",
   },
+  {
+    cover: "assets/img/coverPortal.jpg",
+    video: "assets/img/portal.gif",
+    url: "//yiwenl.github.io/SketchesXR/experiments/portal/dist/index.html",
+    title: "Portal",
+  },
 ];
 
 module.exports = ExpModel;
