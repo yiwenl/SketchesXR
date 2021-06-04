2bbf7122910cde24373aa24f51cec1dcf84c80bb
Yi-Wen Lin <bongiovi015@gmail.com>
2021-06-04 15:06:28 +0100

    add about index page

--- dev/tasks/createProject.js
+++ dev/tasks/createProject.js
@@ -56,6 +56,20 @@ loadIndexFile()
       const pathTarget = `${folderPath}/index.html`;
       writeFile(str, pathTarget, expTitle, expURL, expCover);
     });
+
+    // update about page
+    let strAbout = replace(str, "../../static", "../static");
+    strAbout = replace(strAbout, "../../manifest.json", "../manifest.json");
+    strAbout = replace(strAbout, "../../favicon.ico", "../favicon.ico");
+
+    const pathAbout = `../about/index.html`;
+    fs.writeFile(pathAbout, strAbout, (err, data) => {
+      if (err) {
+        console.log("Error Writing File", pathAbout, err);
+      } else {
+        console.log("Done");
+      }
+    });
   })
   .catch((e) => {
     console.log("Error", e);
