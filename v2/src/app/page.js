7a4470a26068137d85035d8a853413cc49d29a3a
Wen <bongiovi015@gmail.com>
2023-09-30 08:06:13 +0100

    v2 home page

---
+++ v2/src/app/page.js
@@ -0,0 +1,10 @@
+// import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Sketches - XR</h1>
+    </main>
+  );
+}
