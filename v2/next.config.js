62913046105ff46dc03ddb44accefe437c774651
Wen <bongiovi015@gmail.com>
2023-09-30 11:06:35 +0100

    v2 test build

--- v2/next.config.js
+++ v2/next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: "export",
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
