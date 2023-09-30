af5f3278d47a87bcf28ec2780d0778b22cef4643
Wen <bongiovi015@gmail.com>
2023-09-30 09:40:19 +0100

    add header and about page

---
+++ v2/src/app/model/data.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    cover: "assets/img/coverTypography.jpg",
+    video: "assets/img/typography01.gif",
+    url: "//yiwenl.github.io/SketchesXR/experiments/typography01/dist/index.html",
+    title: "Typography",
+  },
+  {
+    cover: "assets/img/coverPortal.jpg",
+    video: "assets/img/portal.gif",
+    url: "//yiwenl.github.io/SketchesXR/experiments/portal/dist/index.html",
+    title: "Portal",
+  },
+  {
+    cover: "assets/img/coverTypography02.jpg",
+    video: "assets/img/typography02.gif",
+    url: "//yiwenl.github.io/SketchesXR/experiments/typography02/dist/index.html",
+    title: "Typography",
+  },
+  {
+    cover: "assets/img/coverZongzi.jpg",
+    video: "assets/img/zongzi.gif",
+    url: "//yiwenl.github.io/SketchesXR/experiments/zongzi/dist/index.html",
+    title: "Zong",
+  },
+  {
+    cover: "assets/img/coverFirefly.jpg",
+    video: "assets/img/firefly.gif",
+    url: "//yiwenl.github.io/SketchesXR/experiments/firefly/dist/index.html",
+    title: "Fireflies",
+  },
+  {
+    cover: "assets/img/coverTunnel.jpg",
+    video: "assets/img/tunnel.gif",
+    url: "//yiwenl.github.io/SketchesXR/experiments/tunnel/dist/index.html",
+    title: "Tunnel",
+  },
+];
--- v2/src/app/page.js
+++ v2/src/app/page.js
@@ -1,10 +1,31 @@
 // import Image from "next/image";
 import styles from "./page.module.css";
+import SiteData from "./model/data";
 
-export default function Home() {
+// components
+import Header from "./components/Header";
+
+export default async function Home() {
+  const { experiments } = await getData();
+
+  const data = experiments.concat();
+  data.reverse();
+
   return (
     <main className={styles.main}>
+      <Header />
       <h1>Sketches - XR</h1>
+      <hr />
+
+      {data.map((item, index) => {
+        return <p key={index}>{item.title}</p>;
+      })}
     </main>
   );
+}
+
+async function getData() {
+  return {
+    experiments: SiteData,
+  };
 }
